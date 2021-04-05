f713963866521e76caafe64a4351cb0fa040facd
J0l-lN-TiT0R <J0l-lN_TiT0R@pm.me>
2021-04-05 15:59:06 +0300

    added a new branch with classic 90 degrees turns

--- Level/Player/Player.gd
+++ Level/Player/Player.gd
@@ -3,61 +3,26 @@ extends KinematicBody2D
 
 var speed = 100
 var velocity = Vector2.ZERO
-var last_velocity = velocity
-var acceleration = Vector2.ZERO
-var last_acceleration = acceleration
-var d_acc = acceleration - last_acceleration
-var friction = 0.9
-var angle
-var rotation_speed = 1.8
-
+	
 	
 func get_input():
 #	velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -1
-		acceleration.x -= 2
+		velocity = Vector2(-1, 0)
+		rotation_degrees = -90
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 1
-		acceleration.x += 2
+		velocity = Vector2(1, 0)
+		rotation_degrees = 90
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = -1
-		acceleration.y -= 2
+		velocity = Vector2(0, -1)
+		rotation_degrees = 0
 	if Input.is_action_pressed("ui_down"):
-		velocity.y = 1
-		acceleration.y += 2
+		velocity = Vector2(0, 1)
+		rotation_degrees = 180
 	
 
-func _physics_process(delta):
+func _process(delta):
 	get_input()
 	
-	# Trying to make decent acceleration
-	if acceleration.x - last_acceleration.x == 0:
-		acceleration *= 0.99
-#	if acceleration.x > -1 and acceleration.x < 1:
-#		acceleration.x = 0
-
-	if acceleration.y - last_acceleration.y == 0:
-		acceleration *= 0.99
-#	if acceleration.y > -1 and acceleration.y < 1:
-#		acceleration.y = 0
-
-
-#	if velocity == Vector2.ZERO:
-#		acceleration = 1	
-#	elif velocity == last_velocity:
-#		print("Gotcha")
-#		acceleration *= 1.01
-#	elif velocity == last_velocity * -1:
-#		acceleration = 1
-#	else:
-#		acceleration /= 1.01
-		
-#	acceleration *= friction
-	print(acceleration)
-	
-	move_and_slide(velocity.normalized() * speed + acceleration)
-	look_at(velocity)
-	
-	last_velocity = velocity
-	last_acceleration = acceleration
+	move_and_slide(velocity * speed)
+#	rotation = velocity.angle()
