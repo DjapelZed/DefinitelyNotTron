40d05760bdf51ff30d15be9a84b983926acab266
Maksim Kushnirov <maks.kushnirov@gmail.com>
2021-04-10 00:27:31 +0300

    reworked movement

--- Level/Player/Player.gd
+++ Level/Player/Player.gd
@@ -1,63 +1,60 @@
 extends KinematicBody2D
- 
 
-var speed = 100
-var velocity = Vector2.ZERO
-var last_velocity = velocity
-var acceleration = Vector2.ZERO
-var last_acceleration = acceleration
-var d_acc = acceleration - last_acceleration
-var friction = 0.9
-var angle
-var rotation_speed = 1.8
+# 1. Right/Left keys - rotate a bike
+# 2. Up/Down - changes a velocity 
 
-	
-func get_input():
+
+#var speed = 100
+#var velocity = Vector2.ZERO
+#
+#
+#func get_input():
 #	velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -1
-		acceleration.x -= 2
+#	if Input.is_action_pressed("ui_left"):
+#		rotation_degrees -= 2
+#	if Input.is_action_pressed("ui_right"):
+#		rotation_degrees += 2
+#	if Input.is_action_pressed("ui_up"):
+#		speed += 10
+#	if Input.is_action_pressed("ui_down"):
+#		speed -= 10
+#
+#
+#func _process(delta):
+#	get_input()
+#	velocity = Vector2(1*cos(rotation), -1*sin(rotation))
+##	velocity = Vector2(speed,0).rotated(rotation_degrees)
+##	The bike can only move forwards
+#	if speed <= 0:
+#		speed = 0
+#
+#
+#
+#	print("velocity: ", velocity, " rotation degrees: ", rotation_degrees, " rotation ", rotation)
+#	move_and_slide(velocity * speed)
+
+
+
+export (int) var speed = 200
+export (float) var rotation_speed = 1.5 
+
+var velocity = Vector2()
+var rotation_dir = 0
+
+func get_input():
+	rotation_dir = 0
+#	velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 1
-		acceleration.x += 2
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -1
-		acceleration.y -= 2
+		rotation_dir += 1
+	if Input.is_action_pressed("ui_left"):
+		rotation_dir -= 1
 	if Input.is_action_pressed("ui_down"):
-		velocity.y = 1
-		acceleration.y += 2
-	
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2(speed, 0).rotated(rotation)
 
 func _physics_process(delta):
 	get_input()
-	
-	# Trying to make decent acceleration
-	if acceleration.x - last_acceleration.x == 0:
-		acceleration *= 0.99
-#	if acceleration.x > -1 and acceleration.x < 1:
-#		acceleration.x = 0
-
-	if acceleration.y - last_acceleration.y == 0:
-		acceleration *= 0.99
-#	if acceleration.y > -1 and acceleration.y < 1:
-#		acceleration.y = 0
-
-
-#	if velocity == Vector2.ZERO:
-#		acceleration = 1	
-#	elif velocity == last_velocity:
-#		print("Gotcha")
-#		acceleration *= 1.01
-#	elif velocity == last_velocity * -1:
-#		acceleration = 1
-#	else:
-#		acceleration /= 1.01
 		
-#	acceleration *= friction
-	print(acceleration)
-	
-	move_and_slide(velocity.normalized() * speed + acceleration)
-	look_at(velocity)
-	
-	last_velocity = velocity
-	last_acceleration = acceleration
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
