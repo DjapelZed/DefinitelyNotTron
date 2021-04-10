f707fb52222449872778fc0f7a473ca920524959
J0l-lN-TiT0R <J0l-lN_TiT0R@pm.me>
2021-04-10 12:29:44 +0300

    improved acceleration

--- Level/Player/Player.gd
+++ Level/Player/Player.gd
@@ -46,15 +46,21 @@ func get_input():
 #	velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		rotation_dir += 1
+		speed -= 5
 	if Input.is_action_pressed("ui_left"):
 		rotation_dir -= 1
+		speed -= 5
 	if Input.is_action_pressed("ui_down"):
-		velocity = Vector2(-speed, 0).rotated(rotation)
+		speed -= 10
 	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2(speed, 0).rotated(rotation)
+		speed += 10
 
 func _physics_process(delta):
 	get_input()
+	
+	if speed < 100:
+		speed = 100
 		
+	velocity = Vector2(speed, 0).rotated(rotation)
 	rotation += rotation_dir * rotation_speed * delta
 	velocity = move_and_slide(velocity)
